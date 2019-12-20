7cf28715caa75a38216babaac6f680937514cd36
florenz <moin@flore.nz>
2019-12-20 13:53:59 +0100

    Set initiial mmr points

--- src/views/History.vue
+++ src/views/History.vue
@@ -1,5 +1,14 @@
 <template>
   <div>
+
+    <b-modal
+      id="modal-initial-mmr" title="Enter your initial MMR"
+      @ok="handleMmrUpdate()"
+    >
+      <p class="my-4">As this is your first entry - pleaser enter your current MMR Points</p>
+      <b-form-input v-model="mmrPoints" required type="number" placeholder="eg. 4000" />
+    </b-modal>
+
     <b-row class="mb-4">
       <b-col>
         <FormResult />
@@ -23,12 +32,34 @@
 </template>
 
 <script>
+import { mapState, mapActions } from 'vuex'
 import FormResult from '@/components/FormResult'
 import TableResults from '@/components/TableResults'
 export default {
   name: "History",
   components: {
     FormResult, TableResults
+  },
+  data: () => ({
+    mmrPoints: ''
+  }),
+  computed: {
+    ...mapState('history', [
+      'mmr'
+    ])
+  },
+  methods: {
+    ...mapActions('history', [
+      'setMmr'
+    ]),
+    handleMmrUpdate() {
+      this.setMmr(this.mmrPoints)
+    }
+  },
+  mounted() {
+    if (this.mmr === null) {
+      this.$bvModal.show('modal-initial-mmr')
+    }
   }
 }
 </script>
