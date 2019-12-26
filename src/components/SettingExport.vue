dae4bd8a1cc90dd0566d05d4acb037b261c5304b
florenz <moin@flore.nz>
2019-12-26 21:11:36 +0100

    Add download as txt

--- src/components/SettingExport.vue
+++ src/components/SettingExport.vue
@@ -13,11 +13,26 @@
 
 <script>
 import { mapGetters } from 'vuex'
+import { encodeDiaryData } from '@/helper'
 
 export default {
   name: 'SettingsExport',
   computed: {
     ...mapGetters('history', ['resultsJSON']),
+  },
+  methods: {
+    downloadExportCode() {
+      const exportCode = encodeDiaryData(this.resultsJSON)
+      const d = new Date();
+      const datestring = ("0" + d.getDate()).slice(-2) + "-" + ("0"+(d.getMonth()+1)).slice(-2) + "-" +
+            d.getFullYear() + "_" + ("0" + d.getHours()).slice(-2) + "-" + ("0" + d.getMinutes()).slice(-2);
+
+      var link = document.createElement('a')
+      link.download = `bgdiary-data-export-${datestring}.txt`
+      var blob = new Blob([exportCode], {type: 'text/plain'})
+      link.href = window.URL.createObjectURL(blob)
+      link.click()
+    }
   }
 }
 </script>
