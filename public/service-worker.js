7ecf3315a97b9ecfaa405ba98cd5068d93e92a49
florenz <moin@flore.nz>
2020-01-05 16:02:32 +0100

    ServiceWorker Config

---
+++ public/service-worker.js
@@ -0,0 +1,19 @@
+workbox.setConfig({
+  debug: false,
+});
+
+workbox.precaching.precacheAndRoute([]);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|svg)$/,
+  workbox.strategies.staleWhileRevalidate({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  }),
+);
+
