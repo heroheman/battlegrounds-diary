f2d8fb93955f65eb0d804ccd7b28c12da2480ef2
florenz <moin@flore.nz>
2020-01-20 15:40:43 +0100

    Footer ADjustments

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <b-container>
     <b-row>
-      <b-col class="text-right">
+      <b-col class="text-center">
         <small>
-          &copy; 2019 - <a href="https://flore.nz">Florenz Heldermann</a> -
+          &copy; 2019 - <a href="https://flore.nz">Florenz</a> -
           <router-link to="/privacy">Privacy</router-link> -
           <router-link to="/changelog">Changelog / Roadmap</router-link> -
           <router-link to="/imprint">Impressum / Imprint</router-link> -
