07569c8567dcbe65111057ad6fa04aa62f18ae42
Florenz Heldermann <moin@flore.nz>
2020-03-23 23:29:20 +0100

    change poll link

--- src/views/History.vue
+++ src/views/History.vue
@@ -4,7 +4,7 @@
       <small>Hi - I am currently working on the next version of the diary. Therefore I would like to know more about you - the user. Maybe you have a few minutes to fill out a survey? Thanks!</small>
       <hr>
       <a href=""></a>
-      <b-button size="sm" variant="primary" href="https://forms.gle/s3oABJqVa9Rn8dx89" target="_blank">Take me to the survey!</b-button>
+      <b-button size="sm" variant="primary" href="https://forms.gle/VXM4UWH1whrMsUvn8" target="_blank">Take me to the survey!</b-button>
     </b-alert>
     <b-modal
       id="modal-initial-mmr" title="Enter your initial MMR"
